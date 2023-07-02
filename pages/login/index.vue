2d6183e1c28310462d81c0ad66573b97c66d02e0
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-02 13:42:11 +0900

    fix

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,29 +1,26 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import Link from "../../components/ui/Link.vue"
-
-definePageMeta({
-  layout: 'page',
-})
+import Layout from "../../layouts/page.vue"
 
 const isLoginMode = ref(true) // Login / Sign up
 </script>
 
 <template>
-  <div>
+  <Layout>
     <div class="text-center">
-      <div>
+      <div v-show="isLoginMode.value">
         <div class="text-2xl">Login</div>
         <div>
           または<a @click="isLoginMode.value = false"><Link>新規登録</Link></a>
         </div>
       </div>
-      <div>
+      <div v-show="!isLoginMode.value">
         <div class="text-2xl">Sign up</div>
         <div>
           または<a @click="isLoginMode.value = false"><Link>ログイン</Link></a>
         </div>
       </div>
     </div>
-  </div>
+  </Layout>
 </template>
