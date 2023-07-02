ca4e0f629c1f21f280a134f942f97f09c15f8f76
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-02 14:24:48 +0900

    Login button

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import Link from "../../components/ui/Link.vue"
+import Button from "../../components/ui/Button.vue"
 import Layout from "../../layouts/page.vue"
 
 const data = reactive({
   isLoginMode: true, // Login / Sign up
 })
+
+const login = () => {
+  alert("ToDo")
+}
 </script>
 
 <template>
   <Layout>
-    <div class="text-center">
+    <div class="text-center bg-white">
       <div :class="{
         'hidden': !data.isLoginMode,
       }">
         <div class="text-2xl">Login</div>
+        <div>
+          <div>
+            <label>User ID</label>
+            <input type="text" placeholder="UserID" />
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="password" placeholder="Password" />
+          </div>
+          <div>
+            <Button @click="login">Login</Button>
+          </div>
+        </div>
         <div>
           または<a @click="data.isLoginMode = false"><Link>新規登録</Link></a>
         </div>
